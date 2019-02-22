4752ce2eff44e9229954970a3c5e9a79e6a22100
stormrubio3000 <storm-3000@hotmail.com>
2019-02-22 14:53:13 -0600

    Add project files.

---
+++ ComicStore/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ComicStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Todo: add comic store
+             * Todo: show comic store
+             * Todo: edit comic store
+             * Todo: review comic store
+             * Todo: delete review for store
+             * Todo: delete comic stores
+             * Todo: make members for the comicstore class
+             * Todo: add products
+             * Todo: show products in stock
+             */
+        }
+    }
+}
