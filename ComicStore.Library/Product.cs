0e3fab2b5fd5a0c9af609685be30b7dbedcf5f35
stormrubio3000 <storm-3000@hotmail.com>
2019-02-22 15:59:29 -0600

    The Base skeleton for products is finished.

--- ComicStore.Library/Product.cs
+++ ComicStore.Library/Product.cs
@@ -6,11 +6,51 @@ namespace ComicStore.Library
 {
     class Product
     {
-        /* Todo: Add Product members
-        * Todo: to show all products
-        * Todo: option to show where a product is available
-        * Todo: option to show what products are in a location
-        * Todo: search for product by name
-        */
+
+        private double _Price;
+
+        private string _Name;
+
+        private int _Inventory;
+
+        public double Price
+        {
+            get => _Price;
+            set
+            {
+                if ( value <= 0)
+                {
+                    throw new ArgumentException("Please enter a positive non zero price. ");
+                }
+
+                _Price = value;
+            }
+        }
+
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name of Product must not be empty. ");
+                }
+                _Name = value;
+            }
+        }
+
+        public int Inventory
+        {
+            get => _Inventory;
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("We are out of stock in " + _Name);
+                }
+                _Inventory = value;
+            }
+        }
     }
 }
