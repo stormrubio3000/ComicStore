6bdb81c07bf342c130bacf53ceeb42a7c8910231
storm <storm-3000@hotmail.com>
2019-02-25 12:57:26 -0600

    Added the repository framework.

---
+++ ComicStore.Library/ComiceStoreRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComicStore.Library
+{
+    class ComiceStoreRepository
+    {
+        //handles the add/delete/edit for comicstores and products.
+    }
+}
---
+++ ComicStore.Library/CustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComicStore.Library
+{
+    class CustomerRepository
+    {
+        //Handles the add/delete/edit/search for customer and Orders
+    }
+}
