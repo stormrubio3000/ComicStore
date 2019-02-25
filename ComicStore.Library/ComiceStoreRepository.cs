7f4ef3ca013976665894c5fa72f10f3852d5c5f4
storm <storm-3000@hotmail.com>
2019-02-25 13:34:20 -0600

    updated user stories on the program and started comicstore repository code.

--- ComicStore.Library/ComiceStoreRepository.cs
+++ ComicStore.Library/ComiceStoreRepository.cs
@@ -1,11 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Linq;
 namespace ComicStore.Library
 {
     class ComiceStoreRepository
     {
         //handles the add/delete/edit for comicstores and products.
+        private readonly ICollection<ComicStore> _data;
+
+
+
+        public ComiceStoreRepository(ICollection<ComicStore> data)
+        {
+            _data = data ?? throw new ArgumentNullException(nameof(data));
+        }
+
+
+
+        //search comic
+
+        public IEnumerable<ComicStore> GetComicStore(string search = null)
+        {
+            if (search == null)
+            {
+                foreach (var item in _data)
+                {
+                    yield return item;
+                }
+            }
+            else
+            {
+                foreach (var item in _data.Where(r => r.Name.Contains(search)))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        //add comic 
+
+
+        public void AddComicStore ( ComicStore comicstore)
+        {
+            if (_data.Any(c => c.Name == comicstore.Name))
+            {
+                throw new InvalidOperationException("A Comic Store with that name already exists. ");
+            }
+            _data.Add(comicstore);
+        }
+
+
+        //delete comic 
+
+        public void DeleteComicStore (ComicStore comicstore)
+        {
+            _data.Delete(comicstore);
+        }
+
+
+        //update comic 
+        public void UpdateComicStore (ComicStore comicstore)
+        {
+            DeleteComicStore(comicstore);
+            AddComicStore(comicstore);
+        }
+
+
+        //search product name
+
+        //add product
+
+        //delete product
+
+        //update product name
+
+        // add more product inventory
+
+        //add product to cart
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- ComicStore/Program.cs
+++ ComicStore/Program.cs
@@ -10,12 +10,18 @@ namespace ComicStore
              * Todo: option to add comic store
              * Todo: option to show comic store
              * Todo: option to edit comic store
-             * Todo: option to review comic store
-             * Todo: option to delete review for store
              * Todo: option to delete comic stores
              * Todo: to show all products
-             * Todo: option to show where a product is available
+             * Todo: Add a product
+             * option to update product
              * Todo: option to show what products are in a location
+             * Todo: option to delete product
+             * Todo: Add to product inventory 
+             * Todo: Add customer
+             * Todo: delete customer
+             * Todo: update customer
+             * Todo: Add product to cart
+             * Todo: Show cart and total
              * Todo: option to quit
              * Todo: option to load stored data
              * Todo: option to save data
@@ -24,7 +30,7 @@ namespace ComicStore
 
 
 
-            //finish framework for classes
+
             //add repository clas
             //make console outputs with menu
             //do the testing stuff
