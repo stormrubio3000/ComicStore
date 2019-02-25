4bc1085736160bd133b07f47ece95b71c0d96a90
storm <storm-3000@hotmail.com>
2019-02-25 15:34:12 -0600

    Added while framework for console app and main menu as well as quit feature.

--- ComicStore/Program.cs
+++ ComicStore/Program.cs
@@ -6,6 +6,67 @@ namespace ComicStore
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Comic League United the 7th largest comic supply store in the tri-state area./b");
+
+            while (true)
+            {
+                MainMenu();
+                string choice = "11";
+                try
+                {
+                    choice = Console.ReadLine();
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a valid Input");
+                }
+
+
+                if(choice == "0")
+                {
+                    break;
+                }
+                else if (choice == "1")
+                {
+
+                }
+                else if (choice == "2")
+                {
+
+                }
+                else if (choice == "3")
+                {
+
+                }
+                else if (choice == "4")
+                {
+
+                }
+                else if (choice == "5")
+                {
+
+                }
+                else if (choice == "6")
+                {
+
+                }
+                else if (choice == "7")
+                {
+
+                }
+                else if (choice == "8")
+                {
+
+                }
+                else if (choice == "9")
+                {
+
+                }
+
+
+                choice = "11";
+            }
+
             /* Todo: console needs menu of options
              * Todo: option to add comic store
              * Todo: option to show comic store
@@ -22,7 +83,6 @@ namespace ComicStore
              * Todo: update customer
              * Todo: Add product to cart
              * Todo: Show cart and total
-             * Todo: option to quit
              * Todo: option to load stored data
              * Todo: option to save data
              */
@@ -36,6 +96,22 @@ namespace ComicStore
             //do the testing stuff
             //add saving
             //add loading
+
+       
+        }
+
+        static public  void MainMenu()
+        {
+            Console.WriteLine("1. Edit a store Location. ");
+            Console.WriteLine("2. Show a store Location. ");
+            Console.WriteLine("3. Edit a Product. ");
+            Console.WriteLine("4. Show a Product. ");
+            Console.WriteLine("5. Edit a Customer. ");
+            Console.WriteLine("6. Show a Customer. ");
+            Console.WriteLine("7. Show Cart. ");
+            Console.WriteLine("8. Add Item to Cart. ");
+            Console.WriteLine("9. Remove Item from Cart. ");
+            Console.WriteLine("0. Remove Item from Cart. ");
         }
     }
 }
