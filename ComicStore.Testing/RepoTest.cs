f8c69e776f6658ac322f8fad30b1a0d1c33ddbc0
storm <storm-3000@hotmail.com>
2019-03-06 02:01:42 -0600

    test set up.

---
+++ ComicStore.Testing/RepoTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+
+namespace ComicStore.Testing
+{
+    public class RepoTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
+/*
+
+void AddCart(Project0Context dbContext, string name, int size, int ID);
+void AddCustomer(Project0Context dbContext, string name, string email, string storeid);
+void AddProduct(Project0Context dbContext, string name, decimal price, int size, int store);
+void AddStore(Project0Context dbContext, string name);
+void CheckOut(Project0Context dbContext, string name, int cartid, out decimal total);
+void DeleteCart(Project0Context dbContext, string name, int size, int ID);
+void DeleteCustomer(Project0Context dbContext, string name, string email);
+void DeleteProduct(Project0Context dbContext, string name);
+void DeleteStore(Project0Context dbContext, string name);
+void ShowCart(Project0Context dbContext, string name, int cartid);
+void ShowCustomers(Project0Context dbContext, string name = null);
+void ShowHistory(Project0Context dbContext, string name, string option);
+void ShowProducts(Project0Context dbContext, string name = null);
+void ShowStores(Project0Context dbContext, string name = null);
+void UpdateCustomer(Project0Context dbContext, string name, string email, string oldn, string olde);
+void UpdateProduct(Project0Context dbContext, string name, decimal price, int size, int store, string old);
+void UpdateStore(Project0Context dbContext, string name, string ne);
+bool CheckCartTime(Project0Context dbContext, string cust, int orderid, DateTime curr_order);
+*/
