429ca56a70d588c6435c16763736ac8dab16afeb
storm <storm-3000@hotmail.com>
2019-03-07 15:19:23 -0600

    Added in controller and first model view to implement.

---
+++ ComicStore.WebApp/ViewModel/ComicStoreModelView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComicStore.WebApp.ViewModel
+{
+    public class ComicStoreModelView
+    {
+    }
+}
